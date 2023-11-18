b004b8cdb0e7045eeb95ed16f84cd50ce766a898
doguskiperto <144055162+doguskiperto@users.noreply.github.com>
2023-11-18 07:20:17 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,62 @@
+import random
+
+def generate_random_data():
+    return data
+    data = generate_random_data()
+
+    for item in data:
+    main()
+if __name__ == "__main__":
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+
+main();
+
+}
+    for (let i = 0; i < 10; i++) {
+function main() {
+    return data;
+    data.forEach(item => {
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+function generateRandomData() {
+    const data = [];
+        console.log("Random Number: " + item);
+    });
+}}
+function main() {
+function generateRandomData() {
+
+main();
+    }
+    const data = [];
+    });
+    for (let i = 0; i < 10; i++) {
+
+
+    const data = generateRandomData();
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+}
+    return data;        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    const data = generateRandomData();
+
+}
+    const data = [];
+function generateRandomData() {
+function main() {
+
+}
+main();
+
+    });
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    return data;
