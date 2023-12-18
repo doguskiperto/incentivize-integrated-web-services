0d00f4d2264b22885c8a40e6e3be9e72871b7254
doguskiperto <144055162+doguskiperto@users.noreply.github.com>
2023-12-18 10:31:19 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,62 +1,30 @@
-import random
-
-def generate_random_data():
-    return data
     data = generate_random_data()
-
-    for item in data:
-    main()
 if __name__ == "__main__":
 def main():
+    main()
+def generate_random_data():
+
     data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+
 
         print(f"Random Number: {item}")
-
+import random
+    });
 main();
-
 }
-    for (let i = 0; i < 10; i++) {
-function main() {
-    return data;
-    data.forEach(item => {
-    const data = generateRandomData();
+
         data.push(Math.floor(Math.random() * 100) + 1);
-    }
-
-function generateRandomData() {
-    const data = [];
-        console.log("Random Number: " + item);
-    });
-}}
-function main() {
-function generateRandomData() {
-
-main();
-    }
-    const data = [];
-    });
-    for (let i = 0; i < 10; i++) {
-
-
     const data = generateRandomData();
     data.forEach(item => {
-        data.push(Math.floor(Math.random() * 100) + 1);
-        console.log("Random Number: " + item);
-}
-    return data;        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-    const data = generateRandomData();
-
-}
-    const data = [];
 function generateRandomData() {
+    const data = [];
 function main() {
 
-}
-main();
-
-    });
-    data.forEach(item => {
-    for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);
     return data;
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+
+    }
+}
